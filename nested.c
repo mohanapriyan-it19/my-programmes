0099214a89d10d4e22305b5adf0a4cd4d57288de
mohanapriyan-it19 <mohanapriyan.it19@bitsathy.ac.in>
2019-09-16 16:04:35 +0530

    first commit

---
+++ nested.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int i, j;
+
+    
+    for(i=1; i<=10; i++)
+    {
+        
+        for(j=1; j<=5; j++)
+        {
+            printf("%d\t", (i*j));
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
