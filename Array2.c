6800003f1a0b68d7f596eb6f8b17d1506ab77a47
mohanapriyan-it19 <53468813+mohanapriyan-it19@users.noreply.github.com>
2019-10-17 14:34:25 +0530

    first commit

---
+++ Array2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num[10],i;
+printf("enter the value");
+for(int i=0;i<10;i++)
+{
+scanf("%d\n",&num[i]);
+}
+for(int i=0;i<10;i++)
+{
+printf("%d\t",num[i]);
+}
+}
+
